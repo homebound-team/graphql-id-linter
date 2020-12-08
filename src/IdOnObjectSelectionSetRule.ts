e35f358dfa567e9d04d3815c36b707be6b452492
Stephen Haberman <stephen.haberman@gmail.com>
2020-12-08 10:25:25 -0600

    Lint for 'code' field on enum details. (#5)

--- src/IdOnObjectSelectionSetRule.ts
+++ src/IdOnObjectSelectionSetRule.ts
@@ -1,6 +1,8 @@
 import {
   ASTVisitor,
   GraphQLError,
+  GraphQLNamedType,
+  GraphQLObjectType,
   isListType,
   isNonNullType,
   isObjectType,
@@ -34,11 +36,24 @@ export function IdOnObjectSelectionSetRule(location: string | undefined) {
         }
 
         const possibleFieldsIncludesId = "id" in type.getFields();
-
-        if (possibleFieldsIncludesId && !selectionSetIncludesId(node.selections)) {
+        if (possibleFieldsIncludesId && !selectionSetIncludes(node.selections, "id")) {
           context.reportError(
             new GraphQLError(
               `Missing "id" field in "${type.name}" in ${location}`,
+              node,
+              undefined,
+              undefined,
+              location ? [location] : undefined,
+            ),
+          );
+        }
+
+        // The enum detail pattern is specific pattern we use for enums, if we detect it,
+        // we need the `code` to be there b/c it's used by the apollo type policy as the key.
+        if (isEnumDetailObject(type) && !selectionSetIncludes(node.selections, "code")) {
+          context.reportError(
+            new GraphQLError(
+              `Missing "code" field in "${type.name}" in ${location}`,
               node,
               undefined,
               undefined,
@@ -54,12 +69,11 @@ export function IdOnObjectSelectionSetRule(location: string | undefined) {
 }
 
 /** Given a set of SelectionNodes from a SelectionSet, ensure "id" is included. */
-function selectionSetIncludesId(selectionNodes: readonly SelectionNode[]) {
+function selectionSetIncludes(selectionNodes: readonly SelectionNode[], fieldName: string) {
   return selectionNodes.some(selectionNode => {
     switch (selectionNode.kind) {
       case "Field":
-        return selectionNode.name.value === "id";
-
+        return selectionNode.name.value === fieldName;
       // Fragments have their own selection sets. If they do not include an "id",
       //   we will catch it when we visit those selection sets.
       case "FragmentSpread":
@@ -67,4 +81,19 @@ function selectionSetIncludesId(selectionNodes: readonly SelectionNode[]) {
         return true;
     }
   });
+}
+
+/**
+ * Look for the FooDetail/code/name pattern of our enum detail objects.
+ *
+ * NOTE: Borrowed from graphql-typescript-factories. A helper library may be in order?
+ */
+function isEnumDetailObject(object: GraphQLNamedType): object is GraphQLObjectType {
+  return (
+    object instanceof GraphQLObjectType &&
+    object.name.endsWith("Detail") &&
+    Object.keys(object.getFields()).length >= 2 &&
+    !!object.getFields()["code"] &&
+    !!object.getFields()["name"]
+  );
 }
